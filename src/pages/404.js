2a6486beaa2d03c87e966e58f4e09e6c902baaf1
SEUNGJOO PAEK <atomsjp@gmail.com>
2024-01-08 01:36:02 +0900

    style: 404 페이지 버튼 색 변경

--- src/pages/404.js
+++ src/pages/404.js
@@ -17,7 +17,7 @@ const NotFoundPage = () => {
         </p>
         <div className="mt-6 flex items-center justify-center gap-x-6">
           <Link to="/"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
             홈으로 이동
           </Link>
         </div>
