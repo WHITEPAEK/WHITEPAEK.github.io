4d50adcdf9ef926233af2b47838fe8a8608d1412
SEUNGJOO PAEK <atomsjp@gmail.com>
2024-01-15 04:36:48 +0900

    feat: 메인 페이지 문구 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ const Index = () => {
                   <div className="flex justify-between gap-x-4">
                     <div className="py-0.5 text-xs leading-5 text-gray-500">
                       <span className="font-medium text-gray-900">
-                        창업 (준비 단계)
+                        케레스랩 (대표)
                       </span>
                     </div>
                     <time dateTime="2024-01-01" className="flex-none py-0.5 text-xs leading-5 text-gray-500">
@@ -60,7 +60,7 @@ const Index = () => {
                   <div className="flex justify-between gap-x-4">
                     <div className="py-0.5 text-xs leading-5 text-gray-500">
                       <span className="font-medium text-gray-900">
-                        주식회사 비디 (파트장)
+                        비디 (파트장)
                       </span>
                     </div>
                     <time dateTime="2020-01-01" className="flex-none py-0.5 text-xs leading-5 text-gray-500">
