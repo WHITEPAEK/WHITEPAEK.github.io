2dead42cd2f6ee0c34125df07fbe1bec9d2a137e
SEUNGJOO PAEK <atomsjp@gmail.com>
2024-01-17 17:44:43 +0900

    feat: utterances to giscus migration

--- src/components/comments.js
+++ src/components/comments.js
@@ -4,27 +4,34 @@ export const Comments = () => {
   const commentRef = useRef(null)
 
   useEffect(() => {
-    const utterances = document.createElement("script")
+    const giscus = document.createElement("script")
 
-    const utterancesConfig = {
-      src: "https://utteranc.es/client.js",
-      repo: "WHITEPAEK/whitepaek.github.io",
-      "issue-term": "pathname",
-      theme: "github-light",
-      crossorigin: "anonymous",
-      async: true
+    const giscusConfig = {
+      "src": "https://giscus.app/client.js",
+      "data-repo": "WHITEPAEK/whitepaek.github.io",
+      "data-repo-id": "R_kgDOK__eEA",
+      "data-category": "Comments",
+      "data-category-id": "DIC_kwDOK__eEM4Cceht",
+      "data-mapping": "pathname",
+      "data-strict": "0",
+      "data-reactions-enabled": "1",
+      "data-emit-metadata": "0",
+      "data-input-position": "bottom",
+      "data-theme": "preferred_color_scheme",
+      "data-lang": "ko",
+      "crossorigin": "anonymous",
+      "async": true
     }
 
-    Object.entries(utterancesConfig).forEach(([key, value]) => {
-      utterances.setAttribute(key, value)
+    Object.entries(giscusConfig).forEach(([key, value]) => {
+      giscus.setAttribute(key, value)
     })
 
-    commentRef.current.appendChild(utterances)
+    commentRef.current.appendChild(giscus)
   }, [])
 
   return (
     <div className="pt-16">
-      {/*<hr className="my-0" />*/}
       <div ref={commentRef} />
     </div>
   )
